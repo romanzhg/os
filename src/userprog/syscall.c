5e79ead4855d9fd1ceb2ab2ee647d0d16b81fe35
Hong Zhang <romanzhg@myth27.Stanford.EDU>
2015-02-11 20:42:55 -0800

    read user buffer directly

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -31,8 +31,6 @@ static void close (int fd);
 static pid_t exec (const char *cmd_line);
 
 static int get_arg (const uint8_t *uaddr);
-static bool put_user (uint8_t *udst, uint8_t byte);
-static int get_user (const uint8_t *uaddr);
 static bool get_filename (char* dest, const char* source);
 
 void
@@ -127,8 +125,6 @@ static bool create (const char *file, unsigned initial_size)
 static bool remove (const char *file)
 {
   char file_name[15];
-  //if (!get_filename(file_name, file))
-  //  return false;
   get_filename(file_name, file);
   return filesys_remove (file_name);
 }
@@ -206,7 +202,6 @@ wait (pid_t pid)
 
 static int
 write(int fd, const void *buffer, unsigned length) {
-  // TODO: is there other ways to validate the address?
   if (!validate_address(buffer))
     exit(-1);
     
@@ -216,7 +211,7 @@ write(int fd, const void *buffer, unsigned length) {
 
   unsigned i;
   for (i = 0; i< length; i++)
-    kbuf[i] = get_user(buffer+i);
+    kbuf[i] =  *(((char *)buffer) + i);
 
   int rtn = 0;
   if (fd == 0)
@@ -273,7 +268,7 @@ read (int fd, void *buffer, unsigned length){
       rtn = file_read(file, kbuf, length);
     }
   for (i=0; i< length; i++)
-    put_user(buffer+i, kbuf[i]);
+    *((char *) buffer + i) = kbuf[i];
 
   free(kbuf);
   return rtn;
@@ -284,44 +279,15 @@ get_arg (const uint8_t *uaddr)
 {
   if (!validate_address(uaddr)) 
     exit(-1);
+
   int result;
-  int tmp;
   uint8_t *buf = (uint8_t *) &result;
   int i = 0;
   for (i = 0; i < 4; i++)
     {
-      tmp = get_user(uaddr + i);
-      if (tmp == -1) {
-        return -1;
-      } else
-        buf[i] = tmp;
+      buf[i] = *((uint8_t *)uaddr + i);
     }
   return result;
-}
-
-/* Reads a byte at user virtual address UADDR.
-   UADDR must be below PHYS_BASE.
-   Returns the byte value if successful, -1 if a segfault
-   occurred. */
-static int
-get_user (const uint8_t *uaddr)
-{
-  int result;
-  asm ("movl $1f, %0; movzbl %1, %0; 1:"
-       : "=&a" (result) : "m" (*uaddr));
-  return result;
-}
- 
-/* Writes BYTE to user address UDST.
-   UDST must be below PHYS_BASE.
-   Returns true if successful, false if a segfault occurred. */
-static bool
-put_user (uint8_t *udst, uint8_t byte)
-{
-  int error_code;
-  asm ("movl $1f, %0; movb %b2, %1; 1:"
-       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
-  return error_code != -1;
 }
 
 static bool
@@ -330,13 +296,9 @@ get_filename (char * dest, const char* source)
   if (!validate_address(source)) 
     exit(-1);
 
-  int rtn;
   int i;
   for (i = 0; i < 15; i++) {
-    rtn = get_user((uint8_t *)source + i);
-    if (rtn == -1)
-      exit(-1);
-    dest[i] = (char) rtn;
+    dest[i] = *((char *)source + i);
     if (dest[i] == '\0')
       break;
   }
